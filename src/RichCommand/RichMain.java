7098a8b94ae9906bee5703d22015b8c8e963c371
kiwi.win <547880119@qq.com>
2012-02-10 06:11:47 -0800

    add scheduler for main function

--- src/RichCommand/RichMain.java
+++ src/RichCommand/RichMain.java
@@ -4,6 +4,7 @@ import RichUtil.RichScheduler;
 
 public class RichMain {
     public static void main(String args[]) {
+        System.out.println((char)27+"[01;31m;This text is red."+(char)27+"[00;00m");
         RichScheduler.initialize();
         RichScheduler.schedule();
     }
